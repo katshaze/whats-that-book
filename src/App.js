d9b3b447577d879741eea05a3f68eceaa84f34af
katshaze <katrinamargaret.hayes@gmail.com>
2020-02-10 14:20:55 +1100

    feat: project initial setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>What's that book / Who's that writer</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
