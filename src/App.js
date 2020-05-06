b17775e383e929fdfac15ef68ebbbb9085687e40
katshaze <katrinamargaret.hayes@gmail.com>
2020-05-06 11:53:32 +1000

    fix: show homepage on refresh

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { navigate } from "@reach/router";
 import Search from "./Search";
 
 const App = () => {
+  navigate("/");
+  
   return (
     <div className="app">
       <Search />
